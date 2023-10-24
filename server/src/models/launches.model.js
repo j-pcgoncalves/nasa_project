43067b4f21bc856827a100213db185640bd70903
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-10-24 22:28:18 +0100

    abort launch with DELETE method

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -2,18 +2,9 @@ const launches = new Map();
 
 let latestFlightNumber = 100;
 
-const launch = {
-    flightNumber: 100,
-    mission: "Kepler Exploration X",
-    rocket: "Explorer IS1",
-    launchDate: new Date("December 27, 2030"),
-    target: "Kepler-442 b",
-    customer: ["ZTM", "NASA"],
-    upcoming: true,
-    success: true,
-};
-
-launches.set(launch.flightNumber, launch);
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
 
 function getAllLaunches() {
     return Array.from(launches.values());
@@ -32,7 +23,18 @@ function addNewLaunch(launch) {
     );
 }
 
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+
+    aborted.upcoming = false;
+    aborted.success = false;
+
+    return aborted;
+}
+
 module.exports = {
+    existsLaunchWithId,
     getAllLaunches,
-    addNewLaunch
+    addNewLaunch,
+    abortLaunchById
 };
