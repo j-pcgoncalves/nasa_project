e00f4171f798b2c6c8dbd33b4fb279278ba6460e
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-10-25 19:19:55 +0100

    connect to MongoDB with Mongoose

--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,23 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
+const { secretConfig } = require("./secretConfig");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () => {
+    console.log("MongoDB connection ready!");
+});
+
+mongoose.connection.on("error", (err) => {
+    console.error(err);
+});
+
 async function startServer() {
+    await mongoose.connect(secretConfig.MONGO_URL);
     await loadPlanetsData();
 
     server.listen(PORT, () => {
