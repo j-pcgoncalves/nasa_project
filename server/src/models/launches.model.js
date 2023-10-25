f9cb2d7752d146e797e290e31eda2f1489f0f1a7
João Gonçalves <92524572+j-pcgoncalves@users.noreply.github.com>
2023-10-26 00:10:55 +0100

    save launches to db

--- server/src/models/launches.model.js
+++ server/src/models/launches.model.js
@@ -1,8 +1,17 @@
-// const launches = require("./launches.mongo");
+const launches = require("./launches.mongo");
 
-const launches = new Map();
+const launch = {
+    flightNumber: 100,
+    mission: "Kepler Exploration X",
+    rocket: "Explorer IS1",
+    launchDate: new Date("December 27, 2030"),
+    target: "Kepler-442 b",
+    customers: ["ZTM", "NASA"],
+    upcoming: true,
+    success: true,
+}
 
-let latestFlightNumber = 100;
+saveLaunch(launch);
 
 function existsLaunchWithId(launchId) {
     return launches.has(launchId);
@@ -10,6 +19,14 @@ function existsLaunchWithId(launchId) {
 
 function getAllLaunches() {
     return Array.from(launches.values());
+}
+
+async function saveLaunch(launch) {
+    await launches.updateOne({
+        flightNumber: launch.flightNumber,
+    }, launch, {
+        upsert: true,
+    });
 }
 
 function addNewLaunch(launch) {
